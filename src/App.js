d61dbf8b433933ba37ea68c7046fae79f5ab2130
Albert <albertkepski3@gmail.com>
2024-09-02 10:37:51 +0200

    delete unused files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { GlobalStyles } from "./assets/styles/GlobalStyles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyles/>
+      <div className="App">
+        <header className="App-header">
+          <h1>CV Maker</h1>
+          <ul>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+          </ul>
+          <button>Click</button>
+        </header>
+      </div>
+    </>
+
   );
 }
 
