f5ad7e6f8a5a8a0af2c9c2db0fdb587f7f185ada
Albert <albertkepski3@gmail.com>
2024-09-11 09:58:24 +0200

    file with description of instruction for creating a CV

---
+++ src/assets/data.js
@@ -0,0 +1,7 @@
+export const instruction = [
+  { id: 1, text: "Wybierz motyw, który Ci się podoba" },
+  { id: 2, text: "Wypełnij dane w formularzu" },
+  { id: 3, text: "Możesz podejrzeć postępy w tworzeniu CV" },
+  { id: 4, text: "W każdej chwili możesz zmienić motyw CV" },
+  { id: 5, text: "Zapisz i wydrukuj swoje CV" },
+]
