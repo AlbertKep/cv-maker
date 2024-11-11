f7938acb790f721ece4f56e829ebb291d3c11e98
Albert <albertkepski3@gmail.com>
2024-11-11 11:45:46 +0100

    add slider with cv themes

--- src/pages/themes/Themes.js
+++ src/pages/themes/Themes.js
@@ -1,9 +1,42 @@
+import { ThemesContainer, Heading, ButtonContainer } from './Themes.styled';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import  themes  from "../../assets/data/themes"
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+import 'swiper/css/pagination';
+
+// import required modules
+import { EffectCards } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
+import { Button } from '../../components/templates/Button';
+
 
 const Themes = () => {
   return (
-
-  <div>Themes</div>
-
+      <ThemesContainer>
+        <Heading>Wybierz motyw</Heading>
+        <Swiper
+          effect={'cards'}
+          grabCursor={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[EffectCards, Pagination]}
+          className="mySwiper"
+        >
+          {themes.map((slide) => (
+            <SwiperSlide key={slide.id}>
+              <img src={slide.name} alt=''/>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <ButtonContainer>
+          <Button to="/creator">Dalej</Button>
+        </ButtonContainer>
+      </ThemesContainer>
   )
 }
 
