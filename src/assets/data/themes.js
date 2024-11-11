ba4a1bc94fc5a7c70225fc29a7856a51ee8bceaa
Albert <albertkepski3@gmail.com>
2024-11-11 11:42:16 +0100

    create file with cv images

---
+++ src/assets/data/themes.js
@@ -0,0 +1,10 @@
+import Theme1 from '../images/theme-1.png';
+import Theme2 from '../images/theme-2.png';
+import Theme3 from '../images/theme-3.png';
+
+const themes = [
+  {id: 1, name: Theme1},
+  {id: 2, name: Theme2},
+  {id: 3, name: Theme3},
+]
+export default themes
