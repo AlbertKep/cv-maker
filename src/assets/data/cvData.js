cf094cc2c991a5d70c6dc8a5398f20972695568b
Albert <albertkepski3@gmail.com>
2024-11-21 15:13:48 +0100

    add file with default cv data

---
+++ src/assets/data/cvData.js
@@ -0,0 +1,17 @@
+export const cvData = {
+  // theme: "theme,"
+  name: "Jan",
+  surname: "Kowalski",
+  email: 'jankowalski@gmail.com',
+  phoneNumber: 555666777,
+  // image
+  description: "Napisz w kilku zdaniach coś o sobie",
+  collegeName: "Politechnika Śląska",
+  degreeCourse: "Maszyny i urządzenia energetyczne",
+  companyName: "Google",
+  workStation: "Junior Front-end Developer",
+  workStationDescription: "Tworzenie responsywnych stron internetowych",
+  // skills: [],
+  // hobby: [],
+  footer: "Wyrażam zgodę na przetwarzanie moich danych osobowych dla potrzeb niezbędnych do realizacji procesu rekrutacji (zgodnie z ustawą z dnia 10 maja 2018 roku o ochronie danych osobowych (Dz. Ustaw z 2018, poz. 1000) oraz zgodnie z Rozporządzeniem Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE (RODO)."
+}
