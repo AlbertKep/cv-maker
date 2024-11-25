3218f8e312b7d151b1efd75f96f34da01535e9c7
Albert <albertkepski3@gmail.com>
2024-11-25 15:10:29 +0100

    import cv provider

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Instruction from "./pages/instruction/Instruction";
 import Creator from "./pages/creator/Creator";
 import View from "./pages/view/View";
 import Themes from "./pages/themes/Themes";
+import CvProvider from './providers/CvDataProvider';
 
 
 function App() {
@@ -19,13 +20,16 @@ function App() {
       <GlobalStyles/>
       <div className="App">
         {/* <WelcomeScreen /> */}
+          <CvProvider>
             <Routes >
-              <Route location={location} key={location.key} path="/" element={<Instruction />} />
-              <Route location={location} key={location.key} path="themes" element={<Themes />} />
-              <Route location={location} key={location.key} path="creator" element={<Creator />} />
-              <Route location={location} key={location.key} path="view" element={<View />} />
-              {/* { <Route path="*" element={<NotFound />} />} */}
-            </Routes>
+                <Route location={location} key={location.key} path="/" element={<Instruction />} />
+                <Route location={location} key={location.key} path="themes" element={<Themes />} />
+                <Route location={location} key={location.key} path="creator" element={<Creator />} />
+                <Route location={location} key={location.key} path="view" element={<View />} />
+                {/* { <Route path="*" element={<NotFound />} />} */}
+              </Routes>
+          </CvProvider>
+
       </div>
     </>
   );
